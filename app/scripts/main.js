5cbb363b6bc5b9161f7c543cfdf282fc89ffb908
André Fiedler <andre.fiedler@me.com>
2014-09-25 19:03:07 +0200

    Started work on Design Proposal

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,17 @@
 (function () {
     'use strict';
+    
     console.log('do it!');
+    
+    // go to personal chat
+    /*
+    document.querySelector('#chat').className = 'current';
+    document.querySelector('[data-position="current"]').className = 'left';
+    */
+    
+    // go back to main screen
+    /*
+    document.querySelector('#chat').className = 'right';
+    document.querySelector('[data-position="current"]').className = 'current';
+    */
 }());
