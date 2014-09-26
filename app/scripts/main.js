649b18a212c30a683c48f7fd60b1fc62fbb89daa
Martin Naumann <martin@geekonaut.de>
2014-09-26 08:40:10 +0200

    Adding basic PoC scripts for XMPP messaging

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,9 @@
 (function () {
     'use strict';
     console.log('do it!');
+
+    document.getElementById("sendTest").addEventListener("click", function() {
+        var stanza = new XMPP.Element('message', { to: window.prompt("Send message to?", "somebody@example.org"), type: 'chat'}).c('body').t(window.prompt("Your message", "Hello there!"));
+        client.send(stanza);
+    });
 }());
