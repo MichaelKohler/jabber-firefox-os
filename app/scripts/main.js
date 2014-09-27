fb2c4831a210413acc6dad11d7d64d43e925d87d
André Fiedler <andre.fiedler@me.com>
2014-09-27 10:08:45 +0200

    Some linting

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -157,8 +157,12 @@
     }, false);
 
     // TEST
-    document.getElementById("sendTest").addEventListener("click", function() {
-        var stanza = new XMPP.Element('message', { to: window.prompt("Send message to?", "somebody@example.org"), type: 'chat'}).c('body').t(window.prompt("Your message", "Hello there!"));
+    document.getElementById('sendTest').addEventListener('click', function() {
+        var stanza = new XMPP.Element('message', {
+            to: window.prompt('Send message to?', 'somebody@example.org'), 
+            type: 'chat'
+        }).c('body').t(window.prompt('Your message', 'Hello there!'));
+        
         client.send(stanza);
     });
     
