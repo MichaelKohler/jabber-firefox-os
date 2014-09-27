abfb14d110788bfc5bb442841d49f2e72a9509f2
Michael Kohler <mkohler@picobudget.com>
2014-09-27 22:06:37 +0200

    fix a jslint error

--- app/scripts/chat.js
+++ app/scripts/chat.js
@@ -1,26 +1,26 @@
-'use strict';
+(function(XMPP) {
+    'use strict';
 
-/* global XMPP */
-/* Note these are connection details for a local dev server :) */
-  
-var client = new XMPP.Client({
-    boshURL: window.prompt('Jabber Server URL', 'https://your-server.com:5281/http-bind/'),
-    jid: window.prompt('Your Jabber ID', 'you@example.org'),
-    password: window.prompt('Your password', ''),
-    preferred: 'PLAIN' // <- should check "Sidebar > Connection > Allow Plaintext Login" setting
-});
+    var client = new XMPP.Client({
+        /* Note these are connection details for a local dev server :) */
+        boshURL: window.prompt('Jabber Server URL', 'https://your-server.com:5281/http-bind/'),
+        jid: window.prompt('Your Jabber ID', 'you@example.org'),
+        password: window.prompt('Your password', ''),
+        preferred: 'PLAIN' // <- should check "Sidebar > Connection > Allow Plaintext Login" setting
+    });
 
-client.addListener(
-    'online',
-    function() {
-        console.log('online');
-    }
-);
+    client.addListener(
+        'online',
+        function() {
+            console.log('online');
+        }
+    );
 
-client.addListener(
-    'error',
-    function(e) {
-        console.error(e);
-        alert('Erorr: ' + e);
-    }
-);
+    client.addListener(
+        'error',
+        function(e) {
+            console.error(e);
+            alert('Erorr: ' + e);
+        }
+    );
+}(window.XMPP));
