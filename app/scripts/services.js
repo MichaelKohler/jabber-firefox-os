b0a68b4af5d0f1e7473741dc650b8c32705f551a
Martin Naumann <martin@geekonaut.de>
2014-09-28 17:47:55 +0200

    Filling the Xmpp service with life

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,8 +1,39 @@
 angular.module('jabber.services', [])
 .factory('XmppSvc', function() {
+  var client = null;
+
+  var setupDefaultListeners = function(client) {
+    client.addListener(
+        'online',
+        function() {
+            console.log('online');
+        }
+    );
+
+    client.addListener(
+        'error',
+        function(e) {
+            console.error(e);
+            alert('Error: ' + e);
+        }
+    );
+  };
+
   return {
-    connect: function(jid, password, server) {
-      return true;
+    connect: function(acc) {
+      client = new XMPP.Client({
+          boshURL: acc.serverUrl,
+          jid: acc.jid,
+          password: acc.password,
+          preferred: 'PLAIN' // BOSH only allows plain logins.
+      });
+
+      setupDefaultListeners(client);
+
+      return client;
+    },
+    addListener: function(event, listener) {
+      client.addListener(event, listener);
     }
   }
 });
