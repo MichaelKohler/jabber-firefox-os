f55688b77fd7c975b020499431751cf209966e70
Michael Kohler <mkohler@picobudget.com>
2014-09-29 13:28:26 +0200

    rewrote settings.js and storage.js to Angular services (fixes issue #21 and WIP for #26)

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -36,4 +36,72 @@ angular.module('jabber.services', [])
       client.addListener(event, listener);
     }
   }
+})
+
+.factory('storageSrv', function() {
+    var storageKey = '';
+
+    return {
+        setKey: function(key) {
+          storageKey = key;
+        },
+        get: function(key) {
+            if (storageKey === '') { storageKey = key; }
+            var item = localStorage.getItem(storageKey);
+            var object;
+            try {
+                object = JSON.parse(item);
+            }
+            catch (exception) {
+                console.warn(exception.message);
+                console.warn(item);
+                return {};
+            }
+            return object || {};
+        },
+
+        save: function(object, key) {
+            var keyToSave = key || storageKey;
+            var item = JSON.stringify(object);
+            localStorage.setItem(keyToSave, item);
+        },
+
+        remove: function(key) {
+            localStorage.removeItem(key);
+        }
+    };
+})
+
+.factory('settingsSrv', function(storageSrv) {
+    var settingsKey = "jabber-settings";
+    var storage = storageSrv.load();
+    var settings = storageSrv.get(this.settingsKey);
+    // TODO: use .json or something similar to store the default values
+    var DEFAULTS = {
+        'foo': 'moooo'
+    };
+
+    return {
+        overloadKey: function(key) {
+            settingsKey = key;
+        },
+        set: function(key, value) {
+            settings[key] = value;
+            save();
+        },
+        save: function() {
+            storage.save(settings, settingsKey);
+        },
+        get: function(key) {
+            return settings[key];
+        },
+        remove: function() {
+            delete settings[key];
+            save();
+        },
+        reset: function() {
+            settings = DEFAULTS;
+            save();
+        }
+    };
 });
