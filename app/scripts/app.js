53961437c54ad1dfb7d8e19a9e9b43f9b51ea1a4
André Fiedler <andre.fiedler@me.com>
2014-09-29 22:35:30 +0200

    More L20n

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,17 +1,23 @@
-angular.module('jabber', ['snap', 'ui.router', 'jabber.controllers', 'jabber.services'])
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('contacts', {
-      url: '/contacts',
-      templateUrl: 'templates/contacts.html',
-      controller: 'ContactsCtrl'
-    })
+(function () {
+  'use strict';
+  
+  angular
+    .module('jabber', ['snap', 'ui.router', 'ngL20n', 'jabber.controllers', 'jabber.services'])
+    .config(function($stateProvider, $urlRouterProvider) {
+      $stateProvider
+      
+        .state('contacts', {
+          url: '/contacts',
+          templateUrl: 'templates/contacts.html',
+          controller: 'ContactsCtrl'
+        })
 
-    .state('account', {
-      url: '/account',
-      templateUrl: 'templates/account.html',
-      controller: 'AccountCtrl'
-    });
+        .state('account', {
+          url: '/account',
+          templateUrl: 'templates/account.html',
+          controller: 'AccountCtrl'
+        });
 
-    $urlRouterProvider.otherwise('/contacts');
-}).run();
+      $urlRouterProvider.otherwise('/contacts');
+    }).run();
+})();
