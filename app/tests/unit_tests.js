c3a73dc757bc1e491711153f0389a2cb710abbcc
Michael Kohler <mkohler@picobudget.com>
2014-09-30 01:38:48 +0200

    replaced chai/mocha with Jasmine + rewrote StorageSrv and SettingsSrv tests to Angular style (fixes #28)

--- app/tests/unit_tests.js
+++ app/tests/unit_tests.js
@@ -1,98 +1,128 @@
 (function () {
     'use strict';
-    mocha.setup('bdd');
-    chai.should();
 
-    describe('Unit Tests', function () {
+    describe('Services', function () {
+
+        beforeEach(module('jabber.services'));
+
         describe('Storage', function () {
-            it('Saving string with setting the key first should work', function () {
-                var storage = new StorageHandler('test-jabber-key');
+            var storage = null;
+
+            beforeEach(inject(function(StorageSvc) {
+                storage = StorageSvc;
+            }));
+
+            it('should save string with setting the key in the same call', function () {
+                var content = 'test';
+                storage.save(content, 'test-jabber-key');
+                var result = storage.get('test-jabber-key');
+                expect(result).toEqual(content);
+                storage.remove('test-jabber-key');
+            });
+
+            it('should save string without key when key was set first', function () {
+                storage.setKey('test-jabber-key');
                 var content = 'test';
                 storage.save(content);
                 var result = storage.get('test-jabber-key');
-                result.should.equal(content);
+                expect(result).toEqual(content);
                 storage.remove('test-jabber-key');
             });
-            it('Saving object with setting the key first should work', function () {
-                var storage = new StorageHandler('test-jabber-key');
+
+            it('should save object without key when key was set first', function () {
+                storage.setKey('test-jabber-key');
                 var object = { test: 'bla1' };
                 storage.save(object);
                 var result = storage.get('test-jabber-key');
-                result.should.eql(object);
+                expect(result).toEqual(object);
                 storage.remove('test-jabber-key');
             });
-            it('Getting value without key should work when key was set first', function () {
-                var storage = new StorageHandler('test-jabber-key');
+
+            it('should get value without key when key was set first', function () {
+                storage.setKey('test-jabber-key');
                 var object = { test: 'bla2' };
                 storage.save(object);
                 var result = storage.get();
-                result.should.eql(object);
+                expect(result).toEqual(object);
                 storage.remove('test-jabber-key');
             });
-            it('Getting value with key should work without setting the key first', function () {
-                var storage = new StorageHandler('test-jabber-key');
+
+            it('should get value with key without setting key first', function () {
+                storage.setKey('');
                 var object = { test: 'bla3' };
-                storage.save(object);
-                storage = new StorageHandler();
+                storage.save(object, 'test-jabber-key');
                 var result = storage.get('test-jabber-key');
-                result.should.eql(object);
+                expect(result).toEqual(object);
                 storage.remove('test-jabber-key');
             });
-            it('Getting non-existing key should return an empty object', function () {
-                var storage = new StorageHandler();
+
+            it('should return an empty object when getting a non-existing key', function () {
+                storage.setKey('');
                 var result = storage.get('fooooo');
-                result.should.eql({});
-                storage.remove('fooooo');
+                expect(result).toEqual({});
             });
-            it('Getting a mal-formatted json entry should return an empty object', function () {
+
+            it('should return an empty object when getting a mal-formatted json entry', function () {
                 var malformatted = '{ "foo": asdadasd';
                 localStorage.setItem('foo', malformatted);
-                var storage = new StorageHandler();
                 var result = storage.get('foo');
-                result.should.eql({});
+                expect(result).toEqual({});
                 storage.remove('foo');
             });
-            it('Removing key should work', function () {
-                var storage = new StorageHandler('foo');
-                storage.save('test')
+
+            it('should remove the storage item', function () {
+                storage.save('test', 'foo')
                 storage.remove('foo');
                 var result = storage.get('foo');
-                result.should.eql({});
+                expect(result).toEqual({});
             });
         });
 
         describe('Settings', function () {
-            it('Setting and getting value should work', function () {
-                var settings = new SettingsHandler('jabber-settings-test');
+            var settings = null;
+            var storage = null;
+
+            beforeEach(inject(function(SettingsSvc, StorageSvc) {
+                settings = SettingsSvc;
+                storage = StorageSvc;
+            }));
+
+            it('should return value after getting it', function () {
+                settings.overloadKey('jabber-settings-test');
                 settings.set('foo', 'foobar');
                 var result = settings.get('foo');
-                result.should.eql('foobar');
-                new StorageHandler().remove('jabber-settings-test');
+                expect(result).toBe('foobar');
+                storage.remove('jabber-settings-test');
             });
-            it('Delete value should work', function () {
-                var settings = new SettingsHandler('jabber-settings-test');
-                settings.delete('foo');
+
+            it('should remove the setting', function () {
+                settings.overloadKey('jabber-settings-test');
+                settings.remove('foo');
                 var result = settings.get('foo');
-                chai.expect(result).to.be.undefined;
-                new StorageHandler().remove('jabber-settings-test');
+                expect(result).not.toBeDefined();
+                storage.remove('jabber-settings-test');
             });
-            it('Resetting value should change the stored value', function () {
-                var settings = new SettingsHandler('jabber-settings-test');
+
+            it('should change stored value when resetting settings', function () {
+                settings.overloadKey('jabber-settings-test');
                 settings.set('foo', 'test');
                 settings.reset();
                 var result = settings.get('foo');
-                result.should.not.eql('test');
-                new StorageHandler().remove('jabber-settings-test');
+                expect(result).not.toBe('test');
+                storage.remove('jabber-settings-test');
             });
-            it('Getting non-existing setting should return undefined', function () {
-                var settings = new SettingsHandler('jabber-settings-test');
+
+            it('should return undefined when getting non-existing setting', function () {
+                settings.overloadKey('jabber-settings-test');
                 var result = settings.get('blablablabla');
-                chai.expect(result).to.be.undefined;
-                new StorageHandler().remove('jabber-settings-test');
+                expect(result).not.toBeDefined();
+                storage.remove('jabber-settings-test');
             });
         });
+    });
 
-        describe('ChatHistory', function () {
+    describe('Models', function () {
+        /*describe('ChatHistory', function () {
             it('Appending message to non-existing conversation should create it', function () {
                 var storage = new StorageHandler();
                 storage.remove('jabber-messages-test@example.org--test');
@@ -151,8 +181,6 @@
                 result[0].date.should.eql(message.date);
                 history.deleteAll();
             });
-        });
+        });*/
     });
-
-    mocha.run();
 }());
