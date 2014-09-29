b8302461a5cd688aa756a9d88bf51105b187d9c4
Martin Naumann <martin@geekonaut.de>
2014-09-29 11:02:50 +0200

    Wiring up the contacts

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -7,7 +7,10 @@ angular.module('jabber.controllers', [])
 })
 .controller('ContactsCtrl', function($scope){
   $scope.contacts = [
-    {nick: 'Alice', jid: 'alice@example.org', state: 'online'},
-    {nick: 'Alice', jid: 'alice@example.org', state: 'online'},
+    {nick: 'Alice', jid: 'alice@example.org', status: 'online'},
+    {nick: 'Bob', jid: 'bob@example.org', status: 'away'},
+    {nick: 'Cecil', jid: 'cecil@example.org', status: 'offline'},
   ];
+
+  $scope.status = 'online';
 });
