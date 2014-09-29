1a60f14d945ab44963a844cfbfa5939ff92b5f27
Martin Naumann <martin@geekonaut.de>
2014-09-29 22:20:07 +0200

    ContactsCtrl now w/ status & username

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -5,12 +5,23 @@ angular.module('jabber.controllers', [])
     client = XmppSvc.connect($scope.account);
   }
 })
-.controller('ContactsCtrl', function($scope){
+.controller('ContactsCtrl', function($scope, XmppSvc){
   $scope.contacts = [
     {nick: 'Alice', jid: 'alice@example.org', status: 'online'},
     {nick: 'Bob', jid: 'bob@example.org', status: 'away'},
     {nick: 'Cecil', jid: 'cecil@example.org', status: 'offline'},
   ];
 
-  $scope.status = 'online';
+  $scope.user = XmppSvc.getUserName();
+
+  if(XmppSvc.getStatus() == 'online') {
+    console.log("Getting online contacts");
+    XmppSvc.getContacts(function(contacts) {
+      $scope.$apply(function() {
+        $scope.contacts = contacts;
+      });
+    });
+  }
+
+  $scope.status = XmppSvc.getStatus();
 });
